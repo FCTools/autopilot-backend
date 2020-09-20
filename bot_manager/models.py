2c4eae9478739156b7efb51de536dfc6a67885af
German Yakimov <german13yakimov@gmail.com>
2020-09-20 13:47:12 +0700

    Import all models to models.py

--- bot_manager/models.py
+++ bot_manager/models.py
@@ -3,6 +3,9 @@ Copyright © 2020 FC Tools. All rights reserved.
 Author: German Yakimov
 """
 
-from django.db import models
+from bot_manager.domains.tracker.user import User
+from bot_manager.domains.tracker.campaign import Campaign
+from bot_manager.domains.tracker.offer import Offer
+from bot_manager.domains.tracker.traffic_source import TrafficSource
+from bot_manager.domains.accounts.bot import Bot
 
-# Create your models here.
