bfb3760d4e969e9a78eacde37580ac6c4323d0d5
German Yakimov <german13yakimov@gmail.com>
2020-10-09 19:40:16 +0700

    Rename field campaign_id in site to campaign.

--- bot_manager/domains/tracker/site.py
+++ bot_manager/domains/tracker/site.py
@@ -13,7 +13,7 @@ class Site(models.Model):
 
     site_id = models.CharField(verbose_name="Site ID", max_length=512, null=False, blank=False, )
 
-    campaign_id = models.ForeignKey(to="Campaign", verbose_name="Campaign ID", null=False, blank=False,
+    campaign = models.ForeignKey(to="Campaign", verbose_name="Campaign ID", null=False, blank=False,
                                     on_delete=models.CASCADE, )
 
     name = models.CharField(verbose_name="Name", max_length=512, null=True, blank=False, )
