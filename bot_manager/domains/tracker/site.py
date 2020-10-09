c16819e32093acd61891d465264d11e6924ce8dd
German Yakimov <german13yakimov@gmail.com>
2020-10-09 19:45:02 +0700

    Change site_id and name lengths from 512 to 2048.

--- bot_manager/domains/tracker/site.py
+++ bot_manager/domains/tracker/site.py
@@ -11,12 +11,12 @@ class Site(models.Model):
     This model represents site (source/zone).
     """
 
-    site_id = models.CharField(verbose_name="Site ID", max_length=512, null=False, blank=False, )
+    site_id = models.CharField(verbose_name="Site ID", max_length=2048, null=False, blank=False, )
 
     campaign = models.ForeignKey(to="Campaign", verbose_name="Campaign ID", null=False, blank=False,
                                     on_delete=models.CASCADE, )
 
-    name = models.CharField(verbose_name="Name", max_length=512, null=True, blank=False, )
+    name = models.CharField(verbose_name="Name", max_length=2048, null=True, blank=False, )
 
     status = models.SmallIntegerField(verbose_name="Status (added to wl/bl)", null=False, blank=False, )
 
