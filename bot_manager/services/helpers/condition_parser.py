986b6ab1125cd4ef53830927752235dd1ab1f4d3
German Yakimov <german13yakimov@gmail.com>
2021-01-29 00:08:13 +0700

    Remove useless scripts, database dumps.

--- bot_manager/services/helpers/condition_parser.py
+++ bot_manager/services/helpers/condition_parser.py
@@ -11,7 +11,7 @@ from copy import deepcopy
 
 AVAILABLE_SYMBOLS = ['(', ')', '>', '<', '=', '<=', '>=', '|', '&',
                      '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '-']
-AVAILABLE_VARIABLES = ['revenue', 'cost', 'profit', 'clicks', 'CPC', 'ROI', 'CR', 'EPC', 'leads', 'cpa', 'approve_%']
+AVAILABLE_VARIABLES = ['revenue', 'cost', 'profit', 'clicks', 'CPC', 'ROI', 'CR', 'EPC', 'leads', 'CPA']
 
 
 class ConditionParser:
