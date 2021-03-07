16ff27aaf97591619045ae0bd7af3281f2f4cd2c
German Yakimov <german13yakimov@gmail.com>
2021-03-07 10:51:31 +0700

    [FCTOOLSDEV-51] Remove useless api drafts.

--- web/backend/urls.py
+++ web/backend/urls.py
@@ -9,17 +9,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from bot_manager.views import BotCreator, BotStarter, BotStopper, BotDeleter, BotUpdater, log_view
+from bot_manager.views import log_view
 
 admin.site.site_header = 'FC Tools Autopilot Administration'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('bots/createBot/', BotCreator.as_view(), name='create_bot'),
-    path('bots/startBot/', BotStarter.as_view(), name='start_bot'),
-    path('bots/stopBot/', BotStopper.as_view(), name='stop_bot'),
-    path('bots/deleteBot/', BotDeleter.as_view(), name='delete_bot'),
-    path('bots/editBot/', BotUpdater.as_view(), name='edit_bot'),
 
     path('bots/monitor/', log_view, name='monitor_view'),
 ]
